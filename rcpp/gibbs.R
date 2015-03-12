3cc0aeb67a2eb76f11b9aa944bc985c95becbc1c
david78k <david78k@gmail.com>
2015-03-11 21:47:06 -0400

    gibbs

---
+++ rcpp/gibbs.R
@@ -0,0 +1,18 @@
+library(Rcpp)
+
+gibbs_r <- function(N, thin) {
+  mat <- matrix(nrow = N, ncol = 2)
+  x <- y <- 0
+
+  for (i in 1:N) {
+    for (j in 1:thin) {
+      x <- rgamma(1, 3, y * y + 4)
+      y <- rnorm(1, 1 / (x + 1), 1 / sqrt(2 * (x + 1)))
+    }
+    mat[i, ] <- c(x, y)
+  }
+  mat
+}
+
+#gibbs_r(10, 5)
+sourceCpp("gibbs.cpp")
